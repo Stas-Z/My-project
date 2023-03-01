a100f5e8195850ce24cdfe14c0f45e94b14fc771
Stas-Z <stanislav@zettafoods.ru>
2023-03-01 15:23:33 +0200

    Babel

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -2,6 +2,7 @@ import HtmlWebpackPlugin from 'html-webpack-plugin'
 import webpack from 'webpack'
 import { BuildOptions } from './types/config'
 import MiniCssExtractPlugin from 'mini-css-extract-plugin'
+import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin'
 
 // Функция которая возвращает список плагинов
 export function buildPlugins({
@@ -9,6 +10,7 @@ export function buildPlugins({
   isDev,
 }: BuildOptions): webpack.WebpackPluginInstance[] {
   // Специальный тип для плагинов
+
   return [
     new HtmlWebpackPlugin({
       // Создаёт для сборки фаил index.html и подключает в него все скрпты и другие файлы
@@ -22,5 +24,6 @@ export function buildPlugins({
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev),
     }),
-  ]
+    isDev && new ReactRefreshWebpackPlugin(), // наш hmr плагин
+  ].filter(Boolean)
 }
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,3 +1,4 @@
+import { useTranslation } from 'react-i18next'
 import { Link } from 'react-router-dom'
 import { classNames } from 'shared/lib/classNames/classNames'
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink'
@@ -9,6 +10,7 @@ interface NavbarProps {
 }
 
 export const Navbar = ({ className }: NavbarProps) => {
+  const { t } = useTranslation()
   return (
     <div className={classNames(cls.navbar, {}, [className])}>
       <div className={cls.links}>
@@ -17,10 +19,10 @@ export const Navbar = ({ className }: NavbarProps) => {
           to={'/'}
           className={cls.mainLink}
         >
-          Главная
+          {t('Main Page')}
         </AppLink>
         <AppLink theme={AppLinkTheme.INVERTED} to={'/about'}>
-          О сайте
+          {t('About us')}
         </AppLink>
       </div>
     </div>
