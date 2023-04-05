b3ca8fa592d345e5be54f3189c4ffc7117f151ac
Stas-Z <stanislav@zettafoods.ru>
2023-04-05 17:01:15 +0300

    fix theme for modal window

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -13,7 +13,7 @@ const App = () => {
     <div className={classNames('app', {}, [theme])}>
       <Suspense fallback="">
         <Navbar />
-        <div className="content-page">
+        <div id="app" className="content-page">
           <Sidebar />
           <ErrorBoundary>
             <AppRouter />
