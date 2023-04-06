454aac905d87bf444715e926e013746f4ac002a4
Stas-Z <stanislav@zettafoods.ru>
2023-04-06 19:21:29 +0300

    add pre commit

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,6 +1,4 @@
-import { useState } from 'react'
 import { useTranslation } from 'react-i18next'
-import { Input } from 'shared/ui/Input/Input'
 
 const MainPage = () => {
   const { t } = useTranslation('main')
