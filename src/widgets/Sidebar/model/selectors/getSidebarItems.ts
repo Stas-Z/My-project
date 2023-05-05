520ed19683545f0278ddde37dd2542eb5f60c607
Stas-Z <stanislav@zettafoods.ru>
2023-05-05 22:11:07 +0300

    fix sidebar

---
+++ src/widgets/Sidebar/model/selectors/getSidebarItems.ts
@@ -0,0 +1,42 @@
+import { createSelector } from '@reduxjs/toolkit'
+import { getUserAuthData } from 'entities/User'
+import { RoutPath } from 'shared/config/routeConfig/routeConfig'
+import AboutIcon from 'shared/assets/icons/about-20-20.svg'
+import MainIcon from 'shared/assets/icons/main-20-20.svg'
+import ProfileIcon from 'shared/assets/icons/profile-20-20.svg'
+import ArticleIcon from 'shared/assets/icons/article-20-20.svg'
+import { SidebarItemType } from '../types/sidebar'
+
+export const getSidebarItems = createSelector(getUserAuthData, (userData) => {
+  const sidebarItemsList: SidebarItemType[] = [
+    {
+      path: RoutPath.main,
+      Icon: MainIcon,
+      text: 'Main Page',
+    },
+    {
+      path: RoutPath.about,
+      Icon: AboutIcon,
+      text: 'About us',
+    },
+  ]
+
+  if (userData) {
+    sidebarItemsList.push(
+      {
+        path: RoutPath.profile + userData.id,
+        Icon: ProfileIcon,
+        text: 'Profile page',
+        authOnly: true,
+      },
+      {
+        path: RoutPath.articles,
+        Icon: ArticleIcon,
+        text: 'Articles Page',
+        authOnly: true,
+      },
+    )
+  }
+
+  return sidebarItemsList
+})
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -3,9 +3,10 @@ import { classNames } from 'shared/lib/classNames/classNames'
 import { Button, ButtonSize, ButtonTheme } from 'shared/ui/Button/Button'
 import { LangSwitcher } from 'shared/ui/LangSwitcher/LangSwitcher'
 import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher/ThemeSwitcher'
-import { SidebarItemsList } from 'widgets/Sidebar/model/items'
+import { useSelector } from 'react-redux'
 import { SidebarItem } from '../SidebarItem/SidebarItem'
 import cls from './Sidebar.module.scss'
+import { getSidebarItems } from '../../model/selectors/getSidebarItems'
 
 interface SidebarProps {
   className?: string
@@ -13,6 +14,8 @@ interface SidebarProps {
 
 export const Sidebar = memo(({ className }: SidebarProps) => {
   const [collapsed, setCollapsed] = useState(false)
+  const sidebarItemsList = useSelector(getSidebarItems)
+
   const onToggle = () => {
     setCollapsed((prev) => !prev)
   }
@@ -35,7 +38,7 @@ export const Sidebar = memo(({ className }: SidebarProps) => {
         {collapsed ? '>' : '<'}
       </Button>
       <div className={cls.items}>
-        {SidebarItemsList.map((item) => (
+        {sidebarItemsList.map((item) => (
           <SidebarItem item={item} collapsed={collapsed} key={item.path} />
         ))}
       </div>
