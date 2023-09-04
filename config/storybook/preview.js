e24a1d1b3cab3d7b60daee335719c1f1712fa474
Stas-Z <stanislav@zettafoods.ru>
2023-09-04 16:16:00 +0300

    fix screenshot tests

--- config/storybook/preview.js
+++ config/storybook/preview.js
@@ -4,9 +4,6 @@ import { RouterDecorator } from '../../src/shared/config/storybook/RouterDecorat
 import { StyleDecorator } from '../../src/shared/config/storybook/StyleDecorator/StyleDecorator'
 import { ThemeDecorator } from '../../src/shared/config/storybook/ThemeDecorator/ThemeDecorator'
 import { SuspenseDecorator } from '../../src/shared/config/storybook/SuspenseDecorator/SuspenseDecorator'
-import isLokiRunning from '@loki/is-loki-running'
-import useLokiCaptureDelay from '@/shared/config/storybook/use-loki-capture-delay.hook'
-import { LokiDelayDecorator } from '@/shared/config/storybook/LokiDelayDecorator/LokiDelayDecorator'
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
