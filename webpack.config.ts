f9f1edbe1ebb1a1f5df6225ac8d0ec5f21600ffb
Stas-Z <stanislav@zettafoods.ru>
2023-10-25 11:17:41 +0300

    get api url function

--- webpack.config.ts
+++ webpack.config.ts
@@ -3,7 +3,17 @@ import path from 'path'
 import webpack from 'webpack'
 
 import { buildWebpackConfig } from './config/build/buildWebpackConfig'
-import { BuildEnv, BuildPaths } from './config/build/types/config'
+import { BuildEnv, BuildMode, BuildPaths } from './config/build/types/config'
+
+function getApiUrl(mode: BuildMode, apiUrl?: string) {
+  if (apiUrl) {
+    return apiUrl
+  }
+  if (mode === 'production') {
+    return '/api'
+  }
+  return 'http://localhost:8000'
+}
 
 export default (env: BuildEnv) => {
   const PORT = env?.port || 3000 // Пытаемся получить переменные env, если переменные не заданы,
@@ -11,7 +21,7 @@ export default (env: BuildEnv) => {
 
   const mode = env?.mode || 'development' // Пытаемся получить переменные env, если переменные не заданы, у нас есть дефолтное значение "development".
   const isDev = mode === 'development'
-  const apiUrl = env?.apiUrl || 'http://localhost:8000' // адрес сервера бэкенд
+  const apiUrl = getApiUrl(mode, env?.apiUrl) // адрес сервера бэкенд
 
   const analyze = env?.analyze || false // переменная env для анализа бандла
 
