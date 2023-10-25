3204a39a509e16e11a7649f5b2bbd7fa4eb8df76
Stas-Z <stanislav@zettafoods.ru>
2023-10-25 10:53:09 +0300

    remove div from main page

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -7,11 +7,6 @@ import { Page } from '@/widgets/Page'
 const MainPage = () => {
   const { t } = useTranslation('main')
 
-  return (
-    <Page data-testid="MainPage">
-      <div>123</div>
-      {t('Main Page')}
-    </Page>
-  )
+  return <Page data-testid="MainPage">{t('Main Page')}</Page>
 }
 export default memo(MainPage)
